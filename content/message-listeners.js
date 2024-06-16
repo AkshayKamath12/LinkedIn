954745560f82896d2816b8d3d29e0875c7a5a512
Akshay Kamath <akkamath@ucsc.edu>
2024-06-16 13:00:47 -0700

    testing

--- content/message-listeners.js
+++ content/message-listeners.js
@@ -1,6 +1,7 @@
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   if (message.action === "startScraping") {
+    chrome.storage.local.set({ jobs: [] });
     scrapeLinkedInJobs()
       .then((data) => {})
       .catch((err) => {
